a35e22972565ebd18c18151415af3cf8b8b29bf5
Jorge Luis Pérez <jolisper@gmail.com>
2015-12-14 01:31:44 -0300

    encoding map created

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,5 +1,24 @@
 (ns alphabet-cipher.coder)
 
+(defn- rotate-coll [positions coll]
+  (let [n (mod positions (count coll))]
+    (concat (drop n coll) (take n coll))))
+
+(def alphabet
+  (map char (range (int \a) (inc (int \z)))))
+
+(def alphabet-keys
+  (for [colum alphabet]
+    (for [row alphabet]
+      (symbol (str (str colum) (str row))))))
+
+(def rotated-alphabets
+  (for [rotation (range (count alphabet))]
+    (rotate-coll rotation alphabet)))
+
+(def encoding-map
+  (mapcat #(zipmap %1 %2) alphabet-keys rotated-alphabets))
+
 (defn encode [keyword message]
   "encodeme")
 

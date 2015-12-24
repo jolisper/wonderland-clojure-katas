4c4fb09bd6e18e9de614960bd30982ded518b220
Jorge Luis Pérez <jolisper@gmail.com>
2015-12-24 02:13:03 -0300

    decode test passed!

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,3 +1,4 @@
+
 (ns alphabet-cipher.coder)
 
 (defn- rotate-coll [positions coll]
@@ -26,8 +27,22 @@
                 (map #(clojure.core/keyword (str %1 %2))
                      (seq colum-word) (seq message))))))
 
+(def decoding-keys
+  (for [rotation (range (count alphabet))]
+    (map #(keyword (str %1 %2)) alphabet (rotate-coll rotation alphabet))))
+
+(def decoding-values
+  (for [ch alphabet] (repeat (count alphabet) ch)))
+
+(def decoding-map
+  (zipmap (flatten decoding-keys) (flatten decoding-values)))
+
 (defn decode [keyword message]
-  "decodeme")
+  (let [colum-word (take (count message) (cycle (seq keyword)))]
+    (apply str
+           (map #(% decoding-map)
+                (map #(clojure.core/keyword (str %1 %2))
+                     (seq colum-word) (seq message))))))
 
 (defn decipher [cipher message]
   "decypherme")
